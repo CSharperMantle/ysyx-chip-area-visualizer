8468ad5aa571d369495e5e5964c210c7bd6a7837
Rong "Mantle" Bao <baorong2005@126.com>
2025-02-15 12:55:08 +0800

    Switch back to noto for sans font

--- src/www/theme.ts
+++ src/www/theme.ts
@@ -12,7 +12,7 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: [
-      '"Source Sans 3 Variable"',
+      '"Noto Sans Variable"',
       "system-ui",
       "Avenir",
       "Helvetica",
